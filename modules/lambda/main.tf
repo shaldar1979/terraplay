39d591a8f2b5e1107d75c82adfe2a013acfee228
shaldar1979 <shouvanik.haldar@accenture.com>
2026-01-30 21:41:48 +0530

    updated lambda module main tf with correct iam role arn

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -22,7 +22,7 @@ resource "aws_iam_role_policy_attachment" "lambda_basic" {
 
 resource "aws_iam_role_policy_attachment" "lambda_sqs" {
   role       = aws_iam_role.lambda_exec_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSLambdaSQSQueueExecutionRole"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaSQSQueueExecutionRole"
 }
 
 resource "aws_lambda_function" "this" {
