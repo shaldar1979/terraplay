172bb969b087efcc6c4ec100b0bb4511adc42158
shaldar1979 <shouvanik.haldar@accenture.com>
2026-01-30 21:45:56 +0530

    increased visibility timeout to 120

--- modules/sqs/main.tf
+++ modules/sqs/main.tf
@@ -1,6 +1,8 @@
 resource "aws_sqs_queue" "this" {
-  name = var.queue_name
+  name                      = var.queue_name
+  visibility_timeout_seconds = 120
 }
+
 
 output "queue_arn" {
   value = aws_sqs_queue.this.arn
