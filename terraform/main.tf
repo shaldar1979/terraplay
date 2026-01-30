049af7852cc4c0481698d4d1c7f1205fc8fef9ad
shaldar1979 <shouvanik.haldar@accenture.com>
2026-01-30 19:52:57 +0530

    rerun github action

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
+data "aws_caller_identity" "current" {}
+
 module "app_bucket" {
   source = "../modules/s3-bucket"
 
-  bucket_name       = var.bucket_name
-  enable_versioning = true
-  enable_encryption = true
+  bucket_name = "${var.bucket_name}-${data.aws_caller_identity.current.account_id}"
 
   tags = {
     Environment = "dev"
