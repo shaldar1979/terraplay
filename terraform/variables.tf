e72bff2f1568c9f8a4d0a22ec7a024cd2a34c687
shaldar1979 <shouvanik.haldar@accenture.com>
2026-01-30 21:03:15 +0530

    updated terraform variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,3 +6,21 @@ variable "aws_region" {
 variable "bucket_name" {
   type = string
 }
+
+variable "lambda_name" {
+  description = "Lambda function name"
+  type        = string
+  default     = "demo-lambda"
+}
+
+variable "sqs_name" {
+  description = "SQS queue name"
+  type        = string
+  default     = "demo-sqs-queue"
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
