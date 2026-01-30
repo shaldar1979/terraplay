8e5c00915636cdf9e997c0866c1d52b917984108
shaldar1979 <shouvanik.haldar@accenture.com>
2026-01-30 11:42:37 +0530

    updated correct name of bucket

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
   
   backend "s3" {
-    bucket         = "haldar-terraform-state-bucket-1"
+    bucket         = "haldar-terraform-state-bucket"
     key            = "s3-bucket/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-lock"
