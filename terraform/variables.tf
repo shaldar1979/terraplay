ebcd5cfecef37f7cedfc3ed4c018af15dd5f348a
shaldar1979 <shouvanik.haldar@accenture.com>
2026-01-30 21:04:50 +0530

    removed duplicate aws region

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -18,9 +18,3 @@ variable "sqs_name" {
   type        = string
   default     = "demo-sqs-queue"
 }
-
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
