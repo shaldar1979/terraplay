170626e07937b65038546b3993f6a6434db83931
shaldar1979 <shouvanik.haldar@accenture.com>
2026-02-04 20:55:19 +0530

    updated root main.tf file and rerun env1 github actions

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,8 @@ data "aws_caller_identity" "current" {}
 module "app_bucket" {
   source = "../modules/s3-bucket"
 
-  bucket_name = "${var.bucket_name}-${data.aws_caller_identity.current.account_id}"
+  bucket_name = "${var.bucket_name}-${var.environment}-${data.aws_caller_identity.current.account_id}"
+
 
   tags = {
     Environment = "dev"
