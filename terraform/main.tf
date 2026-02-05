89d3d2b5cc4561dfc6902c2b97bf3436804ba657
shaldar1979 <shouvanik.haldar@accenture.com>
2026-02-05 12:01:58 +0530

    added sns topics for env1

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,3 +33,36 @@ module "lambda_function" {
 
   environment_variables = local.lambda_env_vars[var.environment]
 }
+
+
+provider "aws" {
+  region = var.aws_region
+}
+
+locals {
+  environments = ["env1", "env2"]
+
+  email_address = "shouvanik.haldar@accenture.com"
+}
+
+resource "aws_sns_topic" "env_topics" {
+  for_each = toset(local.environments)
+
+  name = "demo-sns-${each.key}"
+}
+
+
+resource "aws_sns_topic_subscription" "email_subscriptions" {
+  for_each = aws_sns_topic.env_topics
+
+  topic_arn = each.value.arn
+  protocol  = "email"
+  endpoint  = local.email_address
+}
+
+output "sns_topic_arns" {
+  value = {
+    for env, topic in aws_sns_topic.env_topics :
+    env => topic.arn
+  }
+}
