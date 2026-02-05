2248335d12875f0eb9c087d3236e9428379cdab6
shaldar1979 <shouvanik.haldar@accenture.com>
2026-02-05 09:52:16 +0530

    remove dag related terraform components

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -8,20 +8,4 @@ locals {
     }
   }
 
-  dag_variables = {
-    env1 = {
-      api_url  = "https://api.env1.example.com"
-      db_name  = "env1_database"
-    }
-    env2 = {
-      api_url  = "https://api.env2.example.com"
-      db_name  = "env2_database"
-    }
-  }
-  
-  cidr_block = {
-    env1 = "10.10.0.0/16"
-    env2 = "10.20.0.0/16"
-  }
-
 }
